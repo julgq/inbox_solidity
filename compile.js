66a5ecd12bb54cb360a6bf5aa8f1690cb69c6c8e
Julio <julio@Julios-MacBook-Pro.local>
2018-02-18 18:25:57 -0600

    Deploy contract inbox in testing

--- compile.js
+++ compile.js
@@ -1,7 +1,3 @@
-/* Bad practices:
-require('./contracts/Inbox.sol');  // bad!!
-*/
-
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
@@ -9,8 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-// Exportar solo el contrato Inbox.sol
-// console.log(solc.compile(source, 1));
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
-
-
